cdeda1abcf58b01818926c3a601216dd14a80350
doggie <3859395+fubuloubu@users.noreply.github.com>
2023-08-23 20:59:22 -0400

    refactor!: remove `LiveRunner` from default exports

--- silverback/__init__.py
+++ silverback/__init__.py
@@ -1,10 +1,8 @@
 from .application import SilverBackApp
 from .exceptions import CircuitBreaker, SilverBackException
-from .runner import LiveRunner
 
 __all__ = [
     "CircuitBreaker",
-    "LiveRunner",
     "SilverBackApp",
     "SilverBackException",
 ]
