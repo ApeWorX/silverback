125fe6bb92bcd33ef0de9091748c70087444847d
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-11-26 10:43:49 +0100

    chore: unpin ethpm-types

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[
         "click",  # Use same version as eth-ape
         "eth-ape",  # >=0.7.0,<1.0",
-        "ethpm_types>=2.1.0,<3",
+        "ethpm_types",  # Use same version as eth-ape
         "pydantic_settings",  # Use same version as eth-ape
         "taskiq[metrics]>=0.6.0,<0.7.0",
     ],
