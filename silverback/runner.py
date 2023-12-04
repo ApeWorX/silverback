b156c5b5e7f7ac7c0261cd8557e2b1365d5bed64
Mike Shultz <mike@mikeshultz.com>
2023-12-04 16:19:48 -0700

    chore: debug noise

--- silverback/runner.py
+++ silverback/runner.py
@@ -154,7 +154,7 @@ class WebsocketRunner(BaseRunner, ManagerAccessMixin):
 
     async def _block_task(self, block_handler: AsyncTaskiqDecoratedTask):
         sub_id = await self.subscriptions.subscribe(SubscriptionType.BLOCKS)
-        logger.debug(f"Handling blocks via {sub_id}")
+        logger.debug(f"Handling blocks via {sub_id}.")
         print("HELLO WORLD")
 
         async for raw_block in self.subscriptions.get_subscription_data(sub_id):
