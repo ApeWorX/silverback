acf866ff8295e3e4705299beb0cb83662a44cdfb
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2024-04-02 19:22:46 -0400

    refactor!: add data acquisition models to recorder

--- silverback/types.py
+++ silverback/types.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timezone
+from decimal import Decimal
 from enum import Enum  # NOTE: `enum.StrEnum` only in Python 3.11+
-from typing import Protocol
+from typing import Literal, Protocol, get_args
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing_extensions import Self  # Introduced 3.11
 
 
@@ -38,3 +40,26 @@ class SilverbackID(BaseModel):
 class SilverbackStartupState(BaseModel):
     last_block_seen: int
     last_block_processed: int
+
+
+class BaseDatapoint(BaseModel):
+    type: str  # discriminator
+
+    # NOTE: default value ensures we don't have to set this manually
+    time: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+
+ScalarType = bool | int | float | Decimal
+scalar_types = get_args(ScalarType)
+
+
+class ScalarDatapoint(BaseDatapoint):
+    type: Literal["scalar"] = "scalar"
+
+    # NOTE: app-supported scalar value types:
+    data: ScalarType
+
+
+# This is what a Silverback app task must return to integrate properly with our data acq system
+Metrics = dict[str, BaseDatapoint]
+# Otherwise, log a warning and ignore any unconverted return value(s)
