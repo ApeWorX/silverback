c9ffd35639c16fe627c975752581e3e73503185c
Dalena <mimidalena@gmail.com>
2024-11-19 18:32:23 -0500

    chore: update lint and pre-commit

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ extras_require = {
         "hypothesis-jsonschema",  # Generate strategies for pydantic models
     ],
     "lint": [
-        "black>=24",  # Auto-formatter and linter
-        "mypy>=1.10",  # Static type analyzer
+        "black>=24.10.0,<25",  # Auto-formatter and linter
+        "mypy>=1.13.0,<2",  # Static type analyzer
         "types-setuptools",  # Needed for mypy type shed
-        "flake8>=7",  # Style linter
-        "isort>=5.13",  # Import sorting linter
-        "mdformat>=0.7",  # Auto-formatter for markdown
+        "flake8>=7.1.1,<8",  # Style linter
+        "isort>=5.13.2,<6",  # Import sorting linter
+        "mdformat>=0.7.18",  # Auto-formatter for markdown
         "mdformat-gfm>=0.3.6",  # Needed for formatting GitHub-flavored markdown
         "mdformat-frontmatter>=2.0",  # Needed for frontmatters-style headers in issue templates
         "mdformat-pyproject>=0.0.1",  # Allows configuring in pyproject.toml
