d43ebe13aa7186f4025ee5ead6316fb1526b7dbe
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2025-02-26 20:19:37 -0500

    feat(cli): add ability to use globbed names for some commands (#214)

--- silverback/_click_ext.py
+++ silverback/_click_ext.py
@@ -1,7 +1,8 @@
+import fnmatch
 from datetime import datetime, timedelta
 from functools import update_wrapper
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import click
 from fief_client import Fief
@@ -24,6 +25,20 @@ if TYPE_CHECKING:
 
 # NOTE: only load once
 settings = ProfileSettings.from_config_file()
+
+
+def parse_globbed_arg(selection: str, collection: dict[str, Any]) -> list[Any]:
+    if selection in collection:
+        return [collection[selection]]
+
+    elif matches := fnmatch.filter(collection, selection):
+        return [collection[match] for match in matches]
+
+    elif choices := "', '".join(collection):
+        raise click.BadArgumentUsage(f"Selection '{selection}' should match one of: '{choices}'")
+
+    else:
+        raise click.BadArgumentUsage("No choices available")
 
 
 def cls_import_callback(ctx, param, cls_name):
