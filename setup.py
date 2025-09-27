45194cd5d47a780a717171425141cd16e0f0894f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-27 19:28:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "apepay>=0.3.5,<1",
+        "cchecksum>=0.0.3",  # lower pin only, `eth-ape` governs upper pin
         "click",  # Use same version as eth-ape
         "eth-ape>=0.8.31,<1",
         "ethpm-types>=0.6.10",  # lower pin only, `eth-ape` governs upper pin
