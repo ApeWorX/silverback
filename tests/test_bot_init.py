6c94775adab77a19278900193d442d4c16eec257
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2025-09-30 11:56:15 -0400

    refactor: fix tests

--- tests/test_bot_init.py
+++ tests/test_bot_init.py
@@ -6,9 +6,7 @@ from silverback.exceptions import NoSignerLoaded
 
 def test_signer_required_with_signer_available(settings, signer):
     bot = SilverbackBot(settings=settings(signer), signer_required=True)
-    assert bot.signer_required is True
     assert bot.signer is signer
-    assert hasattr(bot.signer, "call")
 
 
 def test_signer_required_raises_when_no_signer(settings):
@@ -18,5 +16,4 @@ def test_signer_required_raises_when_no_signer(settings):
 
 def test_signer_not_required_default(settings):
     bot = SilverbackBot(settings=settings(None))
-    assert bot.signer_required is False
     assert bot.signer is None
