04f351891735bd89461df805c57839e8fb73b8ef
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2026-02-13 21:07:59 -0500

    feat(Runner): support multiple addresses for event container

--- silverback/main.py
+++ silverback/main.py
@@ -1,4 +1,5 @@
 import atexit
+from collections.abc import Sequence
 import inspect
 from collections import defaultdict
 from datetime import datetime, timedelta
@@ -377,6 +378,7 @@ class SilverbackBot(ManagerAccessMixin):
         self,
         task_type: TaskType,
         container: BlockContainer | ContractEvent | ContractEventWrapper | None = None,
+        from_addresses: Sequence[AddressType | str] | None = None,
         filter_args: dict[str, Any] | None = None,
         cron_schedule: str | None = None,
         metric_name: str | None = None,
@@ -393,6 +395,9 @@ class SilverbackBot(ManagerAccessMixin):
         Args:
             task_type: :class:`~silverback.types.TaskType`: The type of task to create.
             container: (BlockContainer | ContractEvent): The event source to watch.
+            from_addresses: (Sequence[AddressType | str] | None):
+                The set of addresses to filter an anonymous event by.
+                Defaults to none (matches all), ignored if `container` is not anonymous event.
 
         Returns:
             Callable[[Callable], :class:`~taskiq.AsyncTaskiqDecoratedTask`]:
@@ -455,6 +460,11 @@ class SilverbackBot(ManagerAccessMixin):
                     contract, "address"
                 ):
                     labels["address"] = contract.address
+
+                elif from_addresses is not None:
+                    labels["address"] = ",".join(
+                        self.conversion_manager.convert(a, AddressType) for a in from_addresses
+                    )
 
                 labels["event"] = container.abi.signature
 
@@ -602,6 +612,7 @@ class SilverbackBot(ManagerAccessMixin):
     def on_(
         self,
         container: BlockContainer | ContractEvent,
+        from_addresses: Sequence[AddressType | str] | None = None,
         filter_args: dict[str, Any] | None = None,
         **filter_kwargs: dict[str, Any],
     ) -> Callable[[Callable], AsyncTaskiqDecoratedTask]:
@@ -610,6 +621,9 @@ class SilverbackBot(ManagerAccessMixin):
 
         Args:
             container: (BlockContainer | ContractEvent): The event source to watch.
+            from_addresses: (Sequence[AddressType | str] | None):
+                The set of addresses to filter an anonymous event by.
+                Defaults to none (matches all), ignored if `container` is not anonymous event.
             filter_args: (dict[str, Any] | None):
                 Arguments to use for event log filter. Gets combined with ``filter_kwargs``.
                 Is useful for when an event argument name is a Python keyword.
@@ -634,6 +648,7 @@ class SilverbackBot(ManagerAccessMixin):
             return self.broker_task_decorator(
                 TaskType.EVENT_LOG,
                 container=container,
+                from_addresses=from_addresses,
                 filter_args=filter_kwargs,
             )
 
